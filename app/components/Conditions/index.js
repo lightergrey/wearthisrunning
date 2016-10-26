84611a2b90b5e35ed4a6d777c2925211ab29ecdb
Josh Stewart <hi@joshstewart.me>
2016-10-26 13:12:31 -0400

    Update Conditions to take an array instead of immutable list

--- app/components/Conditions/index.js
+++ app/components/Conditions/index.js
@@ -10,30 +10,19 @@ import React from 'react';
 import styles from './styles.css';
 
 function Conditions({ conditions }) {
-  let content;
-
-  if (conditions) {
-    content = (
+  return (
+    <div className={styles.container}>
       <ul className={styles.list}>
         {conditions.map((item, itemIndex) => (
           <li className={styles.item} key={itemIndex}>{item}</li>
         ))}
       </ul>
-    );
-  }
-
-  return (
-    <div className={styles.container}>
-      {content}
     </div>
   );
 }
 
 Conditions.propTypes = {
-  conditions: React.PropTypes.oneOfType([
-    React.PropTypes.object,
-    React.PropTypes.bool,
-  ]).isRequired,
+  conditions: React.PropTypes.array.isRequired,
 };
 
 export default Conditions;
