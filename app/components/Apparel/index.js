af6a6e467dc87a1ee9463c8d1baaaaf6b2ea799e
Josh Stewart <hi@joshstewart.me>
2016-10-26 13:10:51 -0400

    Update Apparel to take an array instead of immutable list

--- app/components/Apparel/index.js
+++ app/components/Apparel/index.js
@@ -10,30 +10,19 @@ import React from 'react';
 import styles from './styles.css';
 
 function Apparel({ apparel }) {
-  let content;
-
-  if (apparel) {
-    content = (
+  return (
+    <div className={styles.container}>
       <ul className={styles.list}>
         {apparel.map((item, itemIndex) => (
           <li key={itemIndex}>{item}</li>
         ))}
       </ul>
-    );
-  }
-
-  return (
-    <div className={styles.container}>
-      {content}
     </div>
   );
 }
 
 Apparel.propTypes = {
-  apparel: React.PropTypes.oneOfType([
-    React.PropTypes.object,
-    React.PropTypes.bool,
-  ]).isRequired,
+  apparel: React.PropTypes.array.isRequired,
 };
 
 export default Apparel;
--- app/components/Apparel/tests/index.test.js
+++ app/components/Apparel/tests/index.test.js
@@ -3,11 +3,10 @@ import Apparel from '../index';
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
-import { fromJS } from 'immutable';
 
 describe('<Apparel />', () => {
   it('Should render a list of item strings', () => {
-    const apparel = fromJS(['foo', 'bar', 'baz']);
+    const apparel = ['foo', 'bar', 'baz'];
     const renderedComponent = shallow(
       <Apparel apparel={apparel} />
     );
